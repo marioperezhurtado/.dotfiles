51e96ad75044d41e51f4e062ddaa006cf1e891b4
Mario Perez Hurtado <marioph10@gmail.com>
2026-05-20 12:38:06 +0200

    tab autocomplete copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,13 +2,4 @@ return {
   "github/copilot.vim",
   name = "copilot",
   lazy = false,
-
-  config = function()
-    vim.g.copilot_no_tab_map = true
-
-    vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-  end,
 }
