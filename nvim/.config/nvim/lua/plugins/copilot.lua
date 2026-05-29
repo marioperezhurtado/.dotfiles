faa99aee2e3a8dd2bfffc60a18821a0fd42c949c
Mario Perez Hurtado <marioph10@gmail.com>
2026-05-29 18:48:37 +0200

    fix: add markdown to copilot suggestions

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,4 +2,17 @@ return {
   "github/copilot.vim",
   name = "copilot",
   lazy = false,
+
+  config = function()
+    vim.g.copilot_filetypes = {
+      markdown = true,
+      mdx = true,
+    }
+
+    vim.filetype.add({
+      extension = {
+        mdx = "mdx",
+      },
+    })
+  end,
 }
