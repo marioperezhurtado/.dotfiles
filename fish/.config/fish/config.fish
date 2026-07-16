2fa3e7fda83d4651e28dc97eb89185e5689c7513
Mario Perez Hurtado <marioph10@gmail.com>
2026-07-16 12:33:34 +0200

    feat: add hyprland auto start flag and opencode alias fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,10 @@ set -Ux EDITOR nvim
 fish_vi_key_bindings
 
 # Start hyprland on tty1
-if test -z "$DISPLAY"; and test (tty) = /dev/tty1
+if set -q AUTO_START_HYPRLAND; and test -z "$DISPLAY"; and test (tty) = /dev/tty1
     exec start-hyprland 
 end
 
 alias la="ls -la"
 alias v="nvim ."
+alias oc="opencode"
